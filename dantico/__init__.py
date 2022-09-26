b1deddb5114a5e7b9bbb1fec55f39b7dd945b466
Max Shapira <maxshapira9@gmail.com>
2022-09-26 14:00:11 +0300

    release version 0.0.9

--- dantico/__init__.py
+++ dantico/__init__.py
@@ -1,6 +1,6 @@
 """Tools to convert Django ORM models to Pydantic models"""
 
-__version__ = "0.0.8"
+__version__ = "0.0.9"
 
 from dantico.factory import SchemaFactory
 from dantico.model_schema import ModelSchema
